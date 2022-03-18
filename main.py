e0bd1a64e8c2067081cf5446062e02d17ff0c324
Piorosen <aoikazto@gmail.com>
2022-03-19 02:14:29 +0900

    headless 기능 추가함.

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ repository_name = "github-Action-Hyomin"
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_argument("lang=ko_KR")
-# options.add_argument('headless')
+options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 options.add_argument("--no-sandbox")
